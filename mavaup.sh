86d62fefb301d82ef99c997295116cee16e4c43a
timityjoe <timityjoe@hotmail.com>
2024-01-19 12:10:16 +0800

    Update
    Update

--- mavaup.sh
+++ mavaup.sh
@@ -2,6 +2,8 @@
 
 echo "Setting up Mava Environment..."
 export MAVA_DIR="/media/timityjoe/DataSSD/workspace/marl/nvidia-rmf-mava"
+source activate base	
+conda deactivate
 conda activate conda39-mava
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 #export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/lib:/usr/lib:/usr/local/lib"
